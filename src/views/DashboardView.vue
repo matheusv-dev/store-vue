b3c0f35f097892c3f3c7ce77ec16cae11f97f483
Matheus Vieira <matheus555vieira@gmail.com>
2024-02-25 18:45:53 -0300

    feat: add page dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,7 +1,30 @@
+
 <template>
-  <div class="home">
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <div class="flex flex-col gap-5 py-3 max-w-screen-x">
+    <div class="flex gap-3 items-center">
+      <v-icon name="fa-cart-arrow-down" scale="2" />
+      <h1 class="text-2xl">Relatório</h1>
+    </div>
+    <div class="grid grid-cols-2 gap-4">
+      <Cart :cartList="carts" />
+    </div>
   </div>
 </template>
+
+<script>
+import { SoldService } from '../service/SoldService';
+import Cart from '../components/Cart.vue'
+
+export default {
+  data() {
+    return {
+      carts: null
+    };
+  },
+  components: { Cart },
+  mounted() {
+    SoldService.getListSoldCarts().then((data) => this.carts = data);
+  },
+
+};
+</script>
